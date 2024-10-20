b1638d8765f25e3974d1ac7ad03b68cc26e94999
VICTUS-15\andre <andrewportillo5530@gmail.com>
2024-10-20 10:27:49 -0600

    se arreglo la distribucion de las columnas

--- src/views/LoginUserView.vue
+++ src/views/LoginUserView.vue
@@ -3,7 +3,7 @@
     <v-container class="pa-0 ma-0 fill-height" fluid>
       <v-row class="no-gutters fill-height">
         <!-- Columna 1 con fondo rojo claro, ocupando el 100% del espacio horizontal -->
-        <v-col class="pa-0 fill-height d-flex" cols="5">
+        <v-col class="pa-0 fill-height d-flex" cols="4">
           <v-sheet class="pa-0 fill-height" color="transparent">
             <div class="ConLogin">
               <v-card
@@ -32,22 +32,14 @@
                 <div class="orange--text text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                     <strong class="text-amber-darken-3">Contraseña</strong>
   
-                  <a
-                    class="text-caption text-decoration-none text-blue"
-                    href="#"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    color=""
-                  >
-                  <strong class="text-amber-darken-3">login password?</strong>
-                  </a>
+               
                 </div>
   
                 <v-text-field
                   :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
                   :type="visible ? 'text' : 'password'"
                   density="compact"
-                  placeholder="Enter your password"
+                  placeholder="ingresa tu contraseña"
                   prepend-inner-icon="mdi-lock-outline"
                   variant="outlined"
                   @click:append-inner="visible = !visible"
@@ -86,7 +78,7 @@
         </v-col>
         
         <!-- Columna 2 con fondo azul claro, ocupando el 100% del espacio horizontal -->
-        <v-col class="pa-0 fill-height d-flex" cols="7">
+        <v-col class="pa-0 fill-height d-flex" cols="8">
           <v-sheet class="pa-0 fill-height caja2" color="blue lighten-4">
             dede
           </v-sheet>
