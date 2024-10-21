8e8c787b7238e195cb4c378d56e7527d42862dbd
VICTUS-15\andre <andrewportillo5530@gmail.com>
2024-10-20 23:28:12 -0600

    se creo la responsividad para todos los elementos de la pagina de login restaurante

--- src/views/LoginRestauranteView.vue
+++ src/views/LoginRestauranteView.vue
@@ -1,112 +1,110 @@
 <template>
-    <v-container class="pa-0 ma-0 fill-height" fluid>
-      <v-row class="no-gutters fill-height">
-        <v-col class="pa-0 fill-height d-flex justify-center align-center" cols="4">
-          <v-sheet class="pa-0 fill-height" color="transparent">
-            <v-card
-              class="mx-auto pa-10 pb-2 fill-height"
-              elevation="2"
-              max-width="400"
-              rounded="md"
+  <v-container class="pa-0 ma-0 fill-height" fluid>
+    <v-row class="no-gutters fill-height">
+      <v-col class="pa-0 fill-height d-flex" cols="4" xs="12" sm="12" md="4" lg="4" xl="4" xxl="4">
+        <v-sheet class="pa-0 fill-height d-flex align-center justify-center" color="transparent">
+          <v-card
+            class="mx-auto pa-10 pb-2 fill-height"
+            elevation="2"
+            max-width="400"
+            rounded="md"
+          >
+            <!-- Contenido del v-card -->
+            <v-img
+              class="mx-auto my-6"
+              max-width="80"
+              src="../assets/logosuper.svg"
+            ></v-img>
+
+            <div class="orange--text text-subtitle-1 text-medium-emphasis">
+              <strong class="text-amber-darken-3">Correo</strong>
+            </div>
+
+            <v-text-field
+              density="compact"
+              placeholder="Ingresa tu correo"
+              prepend-inner-icon="mdi-email-outline"
+              variant="outlined"
+            ></v-text-field>
+
+            <div class="orange--text text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+              <strong class="text-amber-darken-3">Contraseña</strong>
+            </div>
+
+            <v-text-field
+              :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+              :type="visible ? 'text' : 'password'"
+              density="compact"
+              placeholder="ingresa tu contraseña"
+              prepend-inner-icon="mdi-lock-outline"
+              variant="outlined"
+              @click:append-inner="visible = !visible"
+            ></v-text-field>
+
+            <v-card class="mb-12" color="surface-variant" variant="tonal"></v-card>
+
+            <v-btn
+              class="mb-8"
+              color="orange"
+              size="large"
+              variant="tonal"
+              block
+              @click="redirectToAccountUser"
             >
-              <!-- Contenido del v-card -->
-              <v-img
-                class="mx-auto my-6"
-                max-width="80"
-                src="../assets/logosuper.svg"
-              ></v-img>
-  
-              <div class="orange--text text-subtitle-1 text-medium-emphasis">
-                <strong class="text-amber-darken-3">Correo</strong>
-              </div>
-  
-              <v-text-field
-                density="compact"
-                placeholder="Ingresa tu correo"
-                prepend-inner-icon="mdi-email-outline"
-                variant="outlined"
-              ></v-text-field>
-  
-              <div class="orange--text text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                <strong class="text-amber-darken-3">Contraseña</strong>
-              </div>
-  
-              <v-text-field
-                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                :type="visible ? 'text' : 'password'"
-                density="compact"
-                placeholder="ingresa tu contraseña"
-                prepend-inner-icon="mdi-lock-outline"
-                variant="outlined"
-                @click:append-inner="visible = !visible"
-              ></v-text-field>
-  
-              <v-card class="mb-12" color="surface-variant" variant="tonal">
-              </v-card>
-  
-              <v-btn
-                class="mb-8"
-                color="orange"
-                size="large"
-                variant="tonal"
-                block
-                @click="redirectToAccountUser"
+              <strong class="text-amber-darken-3">Iniciar Sesión</strong>
+            </v-btn>
+
+            <v-card-text class="text-center">
+              <span
+                class="text-blue text-decoration-none"
+                @click="redirectToCreateRestaurant"
+                style="cursor: pointer;"
               >
-                <strong class="text-amber-darken-3">Iniciar Sesión</strong>
-              </v-btn>
-  
-              <v-card-text class="text-center">
-                <span
-                  class="text-blue text-decoration-none"
-                  @click="redirectToCreateRestaurant"
-                  style="cursor: pointer;"
-                >
-                  <strong class="text-amber-darken-3">crear restaurante</strong>
-                </span>
-              </v-card-text>
-  
-              <v-card-text class="text-center">
-                <span
-                  class="text-blue text-decoration-none"
-                  @click="redirectToLoginUser"
-                  style="cursor: pointer;"
-                >
-                  <strong class="text-amber-darken-3">iniciar como usuario</strong>
-                </span>
-              </v-card-text>
-            </v-card>
-          </v-sheet>
-        </v-col>
-  
-        <v-col class="pa-0 fill-height d-flex" cols="8">
-          <v-sheet class="pa-0 fill-height caja4"></v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  
-  <script>
-  export default {
-    name: 'LoginUser',
-    data() {
-      return {
-        visible: false // Define la propiedad visible en el data
-      };
+                <strong class="text-amber-darken-3">crear restaurante</strong>
+              </span>
+            </v-card-text>
+
+            <v-card-text class="text-center">
+              <span
+                class="text-blue text-decoration-none"
+                @click="redirectToLoginUser"
+                style="cursor: pointer;"
+              >
+                <strong class="text-amber-darken-3">iniciar como usuario</strong>
+              </span>
+            </v-card-text>
+          </v-card>
+        </v-sheet>
+      </v-col>
+
+      <v-col class="pa-0 fill-height d-flex hide-on-sm-xs-md" cols="8" md="6" lg="6" xl="8" xxl="8">
+        <v-sheet class="pa-0 fill-height caja4"></v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'LoginUser',
+  data() {
+    return {
+      visible: false
+    };
+  },
+  methods: {
+    redirectToLoginUser() {
+      this.$router.push('/'); 
     },
-    methods: {
-      redirectToLoginUser() {
-        this.$router.push('/'); // Cambia la ruta a '/AccountUser'
-      },
-      redirectToCreateRestaurant() {
-        this.$router.push('/crearRestaurante'); // Cambia la ruta a '/AccountUser'
-      }
+    redirectToCreateRestaurant() {
+      this.$router.push('/crearRestaurante'); 
     }
   }
-  </script>
-  
-  <style scoped>
- html, body, #app {
+}
+</script>
+
+<style scoped>
+html, body, #app {
   height: 100%;
   margin: 0;
   padding: 0;
@@ -123,17 +121,18 @@
   align-items: center;
 }
 
-.v-card {
-  max-height: 100%;
-  width: 100%;
-}
+
 
 .caja4 {
-    background-image: url(../assets/res.jpg);
-    background-size: 100vh;
-    background-position: center;
-    background-repeat: no-repeat;
+  background-image: url(../assets/res.jpg);
+  background-size: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
-  </style>
-  
+@media (max-width: 950px) { /* xs and sm breakpoints */
+  .hide-on-sm-xs-md {
+    display: none !important;
+  }
+}
+</style>
