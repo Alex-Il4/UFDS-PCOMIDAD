305224c2828086f72d731ae3cb2f1453afbcbe0a
Adrian-Aguilera <cruz74270@gmail.com>
2024-10-30 22:01:04 -0600

    feat: creacion menu cliente y pagina home

---
+++ src/components/ClienteComponents/SidebarComponent/SideBarComponent.vue
@@ -0,0 +1,61 @@
+
+<template>
+    <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false" color="">
+        <v-list-item prepend-icon="bi bi-person-badge-fill" :title="nombre" nav class="justify-center text-capitalize">
+            <template v-slot:append>
+                <v-btn
+                    icon="bi bi-app"
+                    variant="text"
+                    @click.stop="rail = !rail"
+                ></v-btn>
+                <v-btn :prepend-icon="theme === 'light' ? ' mdi-weather-night' : 'mdi-weather-sunny'"  variant="text" @click="onClick"></v-btn>
+            </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+            <v-list-item prepend-icon="bi bi-shop" title="Inicio" value="home" to="/home"></v-list-item>
+            <v-list-item prepend-icon="bi bi-file-person-fill" title="Perfil" value="account" to="/perfilCliente"></v-list-item>
+            <v-list-item prepend-icon="bi bi-cart-plus-fill" title="Carrito" value="users" to="/carrito"></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
+</template>
+
+<script>
+  export default {
+    name: 'SideBarComponent',
+    props: {
+        theme: {
+            type: String,
+            required: true
+        },
+    },
+    data: () => ({
+      drawer: null,
+      nombre: localStorage.getItem("NombreCliente"),
+      rail: false,
+    }),
+    methods: {
+      onClick() {
+        this.$emit("toggle-theme");
+      },
+      logout() {
+        localStorage.removeItem('access');
+        localStorage.removeItem('refresh');
+        localStorage.removeItem('CorreoCliente');
+        localStorage.removeItem('ClienteID');
+        localStorage.removeItem('NombreCliente');
+        this.$router.push("/login");
+      },
+      onPerfil() {
+        this.$router.push("/perfil");
+      },
+      //funcion que si esta en modo celular se oculte el sibar
+      hideSideBar() {
+        this.drawer = false;
+      }
+    },
+  }
+</script>
