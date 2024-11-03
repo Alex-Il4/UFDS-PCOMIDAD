e83b120c7514f2966a9d1603718e3140ec2ced41
Didi_14 <didicastilloortiz@gmail.com>
2024-11-02 21:51:33 -0600

    agregue un boton para regresar al home

--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,90 +1,102 @@
 <template>
-    <v-container class="perfil">
-      <v-row class="namepic" justify="center">
+  <v-container class="perfil">
+    <v-btn icon @click="$router.push('/')" class="btnHome">
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
+
+    <v-row class="namepic" justify="center">
+      <v-col cols="12" class="text-center">
+        <v-img class="imagen mx-auto" src="../assets/logo.png" alt="Imagen" />
+      </v-col>
+      <v-col cols="12" class="text-center">
+        <h1>{{ user.name }}</h1>
+      </v-col>
+    </v-row>
+
+    <v-container class="container">
+      <v-row class="section" justify="center">
         <v-col cols="12" class="text-center">
-          <v-img class="imagen mx-auto" src="../assets/logo.png" alt="Imagen" />
-        </v-col>
-        <v-col cols="12" class="text-center">
-          <h1>{{ user.name }}</h1>
+          <h2>Información de Perfil</h2><br>
+          <v-list class="info mx-auto">
+            <v-list-item class="infoPerfil" style="text-align: left;">
+              <strong>Nombre:</strong> {{ user.name }} <br>
+              <strong>Email:</strong> {{ user.email }} <br>
+              <strong>Teléfono:</strong> {{ user.tel }} <br>
+              <strong>Dirección:</strong> {{ user.address }} <br>
+            </v-list-item>
+          </v-list>
         </v-col>
       </v-row>
-  
-      <v-container class="container">
-        <v-row class="section" justify="center">
-          <v-col cols="12" class="text-center">
-            <h2>Información de Perfil</h2><br>
-            <v-list class="info mx-auto">
-              <v-list-item class="infoPerfil"  style="text-align: left;">
-                <strong>Nombre:</strong> {{ user.name }} <br>
-                <strong>Email:</strong> {{ user.email }} <br>
-                <strong>Teléfono:</strong> {{ user.tel }} <br>
-                <strong>Dirección:</strong> {{ user.address }} <br>
+
+      <v-row class="section" justify="center">
+        <v-col cols="12" class="text-center">
+          <h2>Historial de Pedidos</h2><br>
+          <v-list class="Listaorden" v-if="orders.length">
+            <v-list-item-group>
+              <v-list-item v-for="order in orders" :key="order.id" class="order" style="text-align: left;">
+                <p><strong>Pedido #{{ order.id }}</strong></p>
+                <p>Fecha: {{ order.date }}</p>
+                <p>Total: ${{ order.total.toFixed(2) }}</p>
               </v-list-item>
-            </v-list>
-          </v-col>
-        </v-row>
-  
-        <v-row class="section" justify="center">
-          <v-col cols="12" class="text-center">
-            <h2>Historial de Pedidos</h2><br>
-            <v-list class="Listaorden" v-if="orders.length">
-              <v-list-item-group>
-                <v-list-item v-for="order in orders" :key="order.id" class="order"  style="text-align: left;">
-                  <p><strong>Pedido #{{ order.id }}</strong></p>
-                  <p>Fecha: {{ order.date }}</p>
-                  <p>Total: ${{ order.total.toFixed(2) }}</p>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-            <p v-else class="noOrders" style="text-align: left;">Aun no tienes pedidos</p>
-          </v-col>
-        </v-row>
-  
-        <v-row class="section actions" justify="space-between">
-          <v-btn @click="editProfile" class="btnEditar">Editar Perfil</v-btn>
-          <v-btn @click="salir" class="btnSalir">Cerrar Sesión</v-btn>
-        </v-row>
-      </v-container>
+            </v-list-item-group>
+          </v-list>
+          <p v-else class="noOrders" style="text-align: left;">Aun no tienes pedidos</p>
+        </v-col>
+      </v-row>
+
+      <v-row class="section actions" justify="space-between">
+        <v-btn @click="editProfile" class="btnEditar">Editar Perfil</v-btn>
+        <v-btn @click="salir" class="btnSalir">Cerrar Sesión</v-btn>
+      </v-row>
     </v-container>
-  </template>
-  
-  <script>
-  export default {
-    name: 'PerfilView',
-    data() {
-      return {
-        user: {
-          name: 'Alguien Cualquiera',
-          email: 'alguien@example.com',
-          tel: '8888-8888',
-          address: 'Calle Cualquiera',
-        },
-        orders: [
-          { id: 1, date: '2024-10-01', total: 25.5 },
-          { id: 2, date: '2024-10-05', total: 40.0 },
-        ],
-      };
+  </v-container>
+</template>
+
+<script>
+export default {
+name: 'PerfilView',
+data() {
+  return {
+    user: {
+      name: 'Alguien Cualquiera',
+      email: 'alguien@example.com',
+      tel: '8888-8888',
+      address: 'Calle Cualquiera',
     },
-    methods: {
-      editProfile() {
-        //editar perfil
-      },
-      salir() {
-        // cerrar Sesion
-      },
-    }
+    orders: [
+      { id: 1, date: '2024-10-01', total: 25.5 },
+      { id: 2, date: '2024-10-05', total: 40.0 },
+    ],
   };
-  </script>
-  
-  <style scoped>
-  .perfil {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 30px;
-    background-color: #f9f9fb;
-    font-family: 'Arial', sans-serif;
-  }
+},
+methods: {
+  editProfile() {
+    //editar perfil
+  },
+  salir() {
+    // cerrar Sesion
+  },
+}
+};
+</script>
+
+<style scoped>
+/* Botón de regreso al inicio */
+.btnHome {
+position: absolute;
+top: 15px;
+left: 15px;
+color: #444;
+}
+
+.perfil {
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 30px;
+background-color: #f9f9fb;
+font-family: 'Arial', sans-serif;
+}
   
   .namepic {
     text-align: center;
