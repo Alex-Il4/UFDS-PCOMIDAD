e84be95ca8b9b23003676e5f5213ef9045aac543
Adrian-Aguilera <cruz74270@gmail.com>
2024-11-04 16:27:51 -0600

    fix: comentarios para restaurantes

--- src/components/ClienteComponents/ComentariosComponents/TableComentariosComponent.vue
+++ src/components/ClienteComponents/ComentariosComponents/TableComentariosComponent.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="border-sm border-info pa-4">
+    <div class="pa-4">
+        <v-divider class="mx-3 mb-1"></v-divider>
         <div v-for="(item, index) in Comentarios" :key="index" class="pa-3">
             <v-alert
                 border="start"
@@ -12,6 +13,8 @@
             <div class="text-caption d-flex justify-end">
                 <v-icon icon="bi bi-person-bounding-box" class="align-self-end mr-2"></v-icon>
                 {{ item.usuario }} • {{ item.fecha }}
+                <v-rating :model-value="item.puntaje" class="ml-2" color="amber" density="compact" size="small" half-increments
+                    readonly></v-rating>
             </div>
           </v-alert>
         </div>
--- src/views/Cliente/Menus/MenusByRestaurantesView.vue
+++ src/views/Cliente/Menus/MenusByRestaurantesView.vue
@@ -4,7 +4,10 @@
             <v-layout>
                 <SideBarComponent :theme="theme" @toggle-theme="onClick"></SideBarComponent>
                 <v-main style="height: 250px" :style="{ height: '100vh', overflowY: 'auto' }">
-                    <v-img class="bg-grey-lighten-2" max-height="200" :src="imagenRestaurante" cover></v-img>
+                    <v-img class="bg-grey-lighten-2" max-height="200" :src="imagenRestaurante" cover>
+                        <v-fab color="orange-darken-4" icon="bi bi-chat-left-dots-fill" app
+                            appear @click="isComentarioDialog = true"></v-fab>
+                    </v-img>
                     <v-data-iterator :items="menusByRestaurante" :items-per-page="4" :search="search">
                         <template v-slot:header>
                             <v-text-field class="px-2 mt-2" v-model="search" density="comfortable"
@@ -46,7 +49,7 @@
                                                 <div class="my-4 text-subtitle-1">
                                                     ${{ item.raw.precio }} • <v-chip
                                                         :color="item.raw.status ? 'success' : 'error'" small>{{
-                                                        item.raw.status }}</v-chip>
+                                                            item.raw.status }}</v-chip>
                                                 </div>
 
                                                 <div>{{ item.raw.descripcion }}</div>
@@ -93,13 +96,53 @@
                             </div>
                         </template>
                     </v-data-iterator>
-                    <div>
-                        <SendComentarioComponent></SendComentarioComponent>
-                        <TableComentariosComponent :Comentarios="comentariosRestaurantes"></TableComentariosComponent>
-                    </div>
-                    <v-snackbar v-model="isActive" :timeout="2000" color="deep-purple-accent-4" elevation="24">
-                        Snackbar with <strong>elevation="24"</strong>.
-                    </v-snackbar>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-alert icon="bi bi-chat-quote-fill" color="info" variant="outlined"
+                                :title="`Comentarios de ${nombreRestaurante}`" class="mr-8 ml-8">
+                            </v-alert>
+                            <TableComentariosComponent :Comentarios="comentariosRestaurantes">
+                            </TableComentariosComponent>
+                        </v-col>
+                    </v-row>
+                    <v-dialog v-model="isComentarioDialog" max-width="600">
+                        <v-card title="Haz un comentario">
+                            <template v-slot:prepend>
+                                <v-icon icon="bi bi-chat-square-dots-fill" size="large" color="warning"></v-icon>
+                            </template>
+                            <v-card-text>
+                                <v-row dense>
+                                    <v-col cols="12">
+                                        <v-text-field v-model="comentarioRestaurante" label="Comentario" type="text"
+                                            variant="outlined" clearable color="warning"
+                                            :rules="[rules.maxlength, rules.required]">
+                                            <template v-slot:prepend>
+                                                <v-icon color="warning">bi bi-chat-quote-fill</v-icon>
+                                            </template>
+                                        </v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-select :items="[1, 2, 3, 4,5]" label="Puntaje*"
+                                            required
+                                            variant="outlined"
+                                            color="warning"
+                                            v-model="puntajeComentario"
+                                            :rules="[rules.required, rules.onlyNumber, rules.puntajeValido]"
+                                        >
+                                            <template v-slot:prepend>
+                                                <v-icon color="warning">bi bi-chat-quote-fill</v-icon>
+                                            </template>
+                                        </v-select>
+                                    </v-col>
+                                </v-row>
+                                <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                    <v-btn text="Cerrar" variant="plain" @click="isComentarioDialog = false"></v-btn>
+                                    <v-btn color="primary" text="Comentar" variant="tonal" @click="sendComentarioRestaurante()" ></v-btn>
+                                </v-card-actions>
+                            </v-card-text>
+                        </v-card>
+                    </v-dialog>
                 </v-main>
             </v-layout>
         </v-app>
@@ -109,14 +152,12 @@
 <script>
 import SideBarComponent from '@/components/ClienteComponents/SidebarComponent/SideBarComponent.vue'
 import TableComentariosComponent from '@/components/ClienteComponents/ComentariosComponents/TableComentariosComponent.vue'
-import SendComentarioComponent from '@/components/ClienteComponents/ComentariosComponents/SendComentarioComponent.vue'
 import axios from 'axios';
 export default {
     name: 'MenusByRestaurantesView',
     components: {
         SideBarComponent,
         TableComentariosComponent,
-        SendComentarioComponent
     },
     data: () => ({
         theme: 'light',
@@ -131,13 +172,24 @@ export default {
         descripcionRestaurante: 'null',
         puntajeRestaurante: 0,
         comentariosRestaurantes: [],
+        rules: {
+            required: value => !!value || 'El campo es requerido',
+            maxlength: value => value.length <= 255 || 'El texto no puede superar los 255 caracteres',
+            onlyNumber: (value) => {
+                //expresion regular solo acepte numeros enteros
+                return /^\d$/.test(value) || 'Solo se perminen numeros'
+            },
+            puntajeValido: (value) => {
+                return /^[1-5]$/.test(value) || 'Solo se permiten valores de 1 a 5'
+            },
+        },
+        comentarioRestaurante: '',
+        puntajeComentario: 0,
+        isComentarioDialog: false,
     }),
     methods: {
         onClick() {
             this.theme = this.theme === 'light' ? 'dark' : 'light';
-        },
-        onScroll() {
-            this.scrollInvoked++
         },
         async loadMenusByRestauranteData() {
             /*Aqui cargamos los menus del restaurante*/
@@ -187,6 +239,7 @@ export default {
             if (respuesta.data) {
                 const restaurante = respuesta.data;
                 this.imagenRestaurante = `${process.env.VUE_APP_API_URL}/restaurantesMethods/api${restaurante.imagen}`;
+                this.nombreRestaurante = restaurante.nombre;
             }
         },
         async loadRestaurante() {
@@ -214,13 +267,40 @@ export default {
                     id: item.id,
                     comentario: item.comentario,
                     fecha: fecha,
-                    usuario: item.usuario.nombre,
+                    usuario: item.usuario ? item.usuario.nombre : 'Usuario sin nombre',
                     imagen: item.imagen ? process.env.VUE_APP_API_URL + item.imagen : null,
-                    color: 'info',
-                    icon: 'mdi-comment-text',
+                    puntaje: item.puntaje,
                 }
             });
             this.comentariosRestaurantes = formatComentarios;
+        },
+
+        async sendComentarioRestaurante() {
+            const json = {
+                "restaurante": this.restauranteID,
+                "usuario": localStorage.getItem("ClienteID"),
+                "comentario": this.comentarioRestaurante,
+                "puntaje": this.puntajeComentario
+            };
+            console.log(json);
+            const headers = {
+                'Authorization': `Bearer ${localStorage.getItem('access')}`,
+                'Content-Type': 'application/json',
+            };
+            try {
+                const response = await axios.post(`${process.env.VUE_APP_API_URL}/comentariosMethods/api/comentarios/restautante-crear/`, json, { headers });
+                const respuesta = response.data.data;
+                console.log(respuesta);
+                if (respuesta) {
+                    this.comentarioRestaurante = '';
+                    this.loadComentariosRestaurantes();
+                    this.isComentarioDialog = false
+                }
+                this.isComentarioDialog = false
+            } catch (error) {
+                console.log(error);
+                this.isComentarioDialog = false
+            }
         },
     },
     created() {
